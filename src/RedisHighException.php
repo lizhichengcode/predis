c09bf7654007c508f713b844049846c4d61bb7de
lizhicheng <lizhichengcode@163.com>
2021-07-01 11:37:12 +0800

    init

---
+++ src/RedisHighException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RedisHigh;
+
+use Exception;
+
+/**
+ * Class RedisHighException
+ * @package RedisHgih
+ */
+class RedisHighException extends Exception
+{
+
+}
